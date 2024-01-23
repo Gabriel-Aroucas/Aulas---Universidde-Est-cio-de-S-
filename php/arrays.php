a48a7959ccabd945a5940a9eab1efdb042d79112
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2024-01-22 21:22:02 -0300

    :sparkles: feat: novas atividades em php

---
+++ php/arrays.php
@@ -0,0 +1,28 @@
+<?php
+    //declaring empty array
+    $carros = array();
+
+    // way to declare an array
+    $carros = array("fusca","monza","passat");
+
+    //another way to declare an array
+    $carros = ["gol","palio","polo"]
+
+    //second way to declare an array
+    $carros[0] = "santana";
+    $carros[1] = "chevette";
+    $carros[2] = "maverick";
+
+    //third way to declare an array
+    $carros[] = "opala";
+    $carros[] = "diplomata";
+    $carros[] = "verona";
+
+
+    //
+    $carros = array(
+        "vw" => "punto",
+        "chevrolet" => "prisma",
+        "fiat" => "tempra"
+    )
+?>
---
+++ php/ola_mundo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    session_start();
+    $arr = array("some-1","some-2","some-3","some-4","some-5","some-6");
+
+  ?>
+  
+<label for="data">Write something</label>
+<input type="text" id="data" name="data">
+</body>
+</html>
