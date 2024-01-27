f6f3c2d366463a5ebb7fe8e37e55d345dc3da423
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2024-01-27 20:53:44 -0300

    :sparkles:feat: query methodos

---
+++ php/consulta_bd/config.php
@@ -0,0 +1,16 @@
+<?php
+    //constants to storage variables
+    define('HOST', 'localhost');
+    define('PORT', '3306');
+    define('DBNAME', 'mock');
+    define('USER', 'root');
+    define('PASSWORD', '');
+
+    try {
+        echo("connected succesfuly");
+        $dsn = new PDO("mysql:host=" . HOST . ";port=" . PORT . ";dbname=" . DBNAME . ";user=" . USER . ";password=" . PASSWORD);
+    } catch (PDOException $err) {
+        echo ("A conexão falhou e retornou o erro: " . $err->getMessage());
+    }
+    // disable connection
+    //$dsn = nulll
--- php/consulta_bd/index.php
+++ php/consulta_bd/index.php
@@ -4,4 +4,20 @@
     function obterAlunos(){
         global $dsn, $user,$pass;
         $alunos = Array();
+
+        try{
+            $pdo =new PDO($dsn,$user,$pass,
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+            $sql = "SELECT * FROM alunos";
+            $stm = $pdo -> prepare($sql);
+            $stm -> execute();
+            $alunos  = $stm -> fetchAll(PDO::FETCH_OBJ);
+        }catch(PDOException $err){
+            $alunos = [];
+        }finally{
+            if($pdo){
+                $pdo = null;
+            }
+        }
+        return $alunos;
     }
